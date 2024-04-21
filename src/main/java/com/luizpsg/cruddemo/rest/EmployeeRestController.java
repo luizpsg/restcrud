c467072213af315441116750927490506754a433
luizpsg <999785@sga.pucminas.br>
2024-04-21 17:28:37 -0300

    Refactor EmployeeRestController to add getEmployee method

--- src/main/java/com/luizpsg/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luizpsg/cruddemo/rest/EmployeeRestController.java
@@ -3,6 +3,8 @@ package com.luizpsg.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,26 @@ public class EmployeeRestController {
   @GetMapping("/employees")
   public List<Employee> findAll() {
     return employeeService.findAll();
+  }
+
+  @GetMapping("/employees/{employeeId}")
+  public Employee getEmployee(@PathVariable int employeeId) {
+    Employee theEmployee = employeeService.findById(employeeId);
+
+    if (theEmployee == null) {
+      throw new RuntimeException("Employee id not found - " + employeeId);
+    }
+
+    return theEmployee;
+  }
+
+  @PostMapping("/employees")
+  public Employee addEmployee(Employee theEmployee) {
+    theEmployee.setId(0);
+
+    employeeService.save(theEmployee);
+
+    return theEmployee;
   }
 
 
